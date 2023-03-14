20f987d3c038e070a3fd0e26aa84b87f114206e6
zamirah morgan <zamorgan2030@gmail.com>
2023-03-14 01:33:00 -0400

    pushing code updates to make card display into a guessing game

--- flash-cards/src/components/CardLists.jsx
+++ flash-cards/src/components/CardLists.jsx
@@ -2,44 +2,76 @@ import { useState } from 'react'
 import FlashCard from "./FlashCard"
 
 const CardLists=(props) => {
-    const [card, setClick] = useState(0);
-    const [anothercard, setAnotherClick] = useState(false);
-    const clickNext=() => setAnotherClick(false);
-
-    const cards=[
-        <FlashCard title="One" description="First Response" answer="Answer" flipped={anothercard} />,
-        <FlashCard title="Two" description="Second Response" answer="Shown Answer" flipped={anothercard} />,
-        <FlashCard title="Three" description="Third Response" answer="Shown Answer" flipped={anothercard} />,
-        <FlashCard title="Four" description="Fourth Response" answer="Shown Answer" flipped={anothercard} />,
-        <FlashCard title="Five" description="Fifth Response" answer="Shown Answer" flipped={anothercard} />,
-    ]
-
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    const [isFlipped, setisFlipped] = useState(false);
+    const [guessResult, setGuessResult] = useState(null);
+    const [inputValue, setInputValue] = useState('');
+  
     const cardText=[
-        {title:"Food", description:"pollo", answer:"chicken", flipped:{setAnotherClick}},
-        {title:"Food", description:"queso", answer:"cheese", flipped:{setAnotherClick}},
-        {title:"Food", description:"cola de buey", answer:"oxtail", flipped:{setAnotherClick}},
-        {title:"Food", description:"arroz y frijoles", answer:"rice", flipped:{setAnotherClick}},
-        {title:"Food", description:"pescado", answer:"fish", flipped:{setAnotherClick}},
-        {title:"Fruit", description:"pera", answer:"pear", flipped:{setAnotherClick}},
-        {title:"Fruit", description:"manzana", answer:"apple", flipped:{setAnotherClick}},
-        {title:"Fruit", description:"uva", answer:"grape", flipped:{setAnotherClick}},
-        {title:"Fruit", description:"naranja", answer:"orange", flipped:{setAnotherClick}},
-        {title:"Fruit", description:"aguacate", answer:"avacado", flipped:{setAnotherClick}},
-    ]
-    const[myCard,setCard]=useState(<FlashCard title={cardText[0].title} description={cardText[0].description} answer={cardText[0].answer} flipped={false} />)
-    const NextFlashCards=() => {
-        const randomCards=Math.floor(Math.random()*cardText.length);
-        setAnotherClick(false);
-        setClick(randomCards);
-        setCard(<FlashCard title={cardText[randomCards].title} description={cardText[randomCards].description} answer={cardText[randomCards].answer} flipped={false} />)
-    }
+        {title:"Food", description:"pollo", answer:"chicken"},
+        {title:"Food", description:"queso", answer:"cheese"},
+        {title:"Food", description:"cola de buey", answer:"oxtail"},
+        {title:"Food", description:"arroz y frijoles", answer:"rice"},
+        {title:"Food", description:"pescado", answer:"fish"},
+        {title:"Fruit", description:"pera", answer:"pear"},
+        {title:"Fruit", description:"manzana", answer:"apple"},
+        {title:"Fruit", description:"uva", answer:"grape"},
+        {title:"Fruit", description:"naranja", answer:"orange"},
+        {title:"Fruit", description:"aguacate", answer:"avocado"},
+    ];
+
+    const handleNext = () => {
+        const nextCardIndex = (currentCardIndex + 1) % cardText.length;
+        setCurrentCardIndex(nextCardIndex);
+        setisFlipped(false);
+        setGuessResult(null);
+        setInputValue('');
+    };
+
+    const handlePrev = () => {
+        const prevCardIndex = (currentCardIndex - 1 + cardText.length) % cardText.length;
+        setCurrentCardIndex(prevCardIndex);
+        setisFlipped(false);
+        setGuessResult(null);
+        setInputValue('');
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const answer = cardText[currentCardIndex].answer.toLowerCase();
+        const guess = inputValue.trim().toLowerCase();
+        if (guess === answer) {
+            setGuessResult('Correct!');            
+        } else {
+            setGuessResult('Incorrect. Please try again.');
+        }
+    };
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
     return (
         <div>
             <h4>Number of Cards: {cardText.length}</h4>
-            {myCard}
-            <button onClick={NextFlashCards}>⭢</button>
+            <FlashCard 
+                title={cardText[currentCardIndex].title}
+                description={cardText[currentCardIndex].description}
+                answer={cardText[currentCardIndex].answer}
+                flipped={isFlipped}
+            />
+            <button onClick={handlePrev} disabled={currentCardIndex === 0}>⭠</button>
+            <button onClick={handleNext} disabled={currentCardIndex === cardText.length - 1}>⭢</button>
+            <form onSubmit={handleSubmit}>
+          <label className="guess-label">
+            Guess the answer here:
+            <input type="text" value={inputValue} onChange={handleChange} />
+          </label>        
+          <button type="submit" className="submit-button">Submit Guess</button>
+        </form>
+        {guessResult && <p>{guessResult}</p>}            
         </div>
-    )
-}
+    );
+};
 
 export default CardLists;
